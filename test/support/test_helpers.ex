6a03f89bf9f5419a768a7c4809f1100792b3e276
Krzysztof Peksa <krzysztof@peksa.pro>
2019-11-27 00:12:13 +0100

    Further attempts to fix problems from tests - profile, follower

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -1,4 +1,6 @@
 defmodule ProfileApi.TestHelpers do
+  import Ecto.Query#, only: [from: 2]
+
   alias ProfileApi.Repo
 
   alias ProfileApi.Social.{Profile, Follower}
@@ -23,5 +25,44 @@ defmodule ProfileApi.TestHelpers do
       |> Repo.insert()
 
     user
+  end
+
+  def profile(name, platform) do
+    Profile
+    |> where([p], p.name == ^name and p.platform == ^platform)
+    |> select([p], {p})
+    |> first
+    |> Repo.one()
+    #from(u in User, where: u.username == name and u.email == platform)
+    #|> Repo.one
+  end
+
+  def profile_fixture(%User{} = user, attrs \\ %{}) do
+    name = "profile-#{System.unique_integer([:positive])}"
+    platform = Enum.random(["facebook","twitter"])
+
+    attrs =
+      Enum.into(attrs, %{
+        name: attrs[:name] || name,
+        platform: attrs[:platform] || platform,
+      })
+    {:ok, profile} =
+      %Profile{}
+      |> Profile.changeset(attrs)
+      |> Ecto.Changeset.put_assoc(:user, user)
+      |> Repo.insert()
+
+    profile
+  end
+
+  def follower_fixture() do
+    inserted_at = NaiveDateTime.truncate(NaiveDateTime.utc_now, :second)
+    updated_at = NaiveDateTime.truncate(NaiveDateTime.utc_now, :second)
+    profile_random = ProfileApi.TestHelpers.user_fixture()
+    follower_random = ProfileApi.TestHelpers.user_fixture()
+    {:ok, follower} =
+      Repo.insert_all(Follower, [%{profile_id: profile_random.id, follower_id: follower_random.id, inserted_at: inserted_at, updated_at: updated_at}])
+
+    follower
   end
 end
