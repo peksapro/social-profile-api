5c1a91b1dc999ab438f39015ae3f9e5a40ecfea3
Krzysztof Peksa <krzysztof@peksa.pro>
2019-11-27 14:12:16 +0100

    added absinthe deps

--- mix.exs
+++ mix.exs
@@ -41,7 +41,10 @@ defmodule ProfileApi.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:pbkdf2_elixir, "~> 1.0"}
+      {:pbkdf2_elixir, "~> 1.0"},
+      {:absinthe, "~> 1.4.2"},
+      {:absinthe_plug, "~> 1.4.0"},
+      {:absinthe_phoenix, "~> 1.4.0"}
     ]
   end
 
